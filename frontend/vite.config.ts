ade5142e57735740935f80732dbd6bd7e0e8bd9a
Aman-web-dev <amanzhx1234+github@gmail.com>
2025-07-12 22:41:16 +0530

    Vite version Error Resolved

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   build:{
     sourcemap:true
   }
